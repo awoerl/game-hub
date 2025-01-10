3ca4a8a2f3041278bf3a2fccdbe109437b51fa10
Alexander Wörl <alexander.woerl@contentserv.com>
2025-01-10 11:54:00 +0100

    Show skeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,30 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+  if (error) {
+    return null;
+  }
+
+  if (isLoading)
+    return skeletons.map((s) => (
+      <HStack marginY="10px" height="32px" marginBottom="10px">
+        <Skeleton borderRadius="8px" width="42px" height="100%" />
+        <Skeleton borderRadius="8px" width="100%" height="100%" />
+      </HStack>
+    ));
 
   return (
     <List>

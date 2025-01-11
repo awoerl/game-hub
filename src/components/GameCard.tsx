42ffa28234649d36028f545ec9c9b44cb4afd90c
Alexander Wörl <alexander.woerl@contentserv.com>
2025-01-11 09:32:37 +0100

    Added emojis to grid

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -26,6 +27,7 @@ const GameCard = ({ game }: Props) => {
           fontSize="2xl"
         >
           {game.name}
+          <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>

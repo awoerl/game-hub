576a007336de5bec4e11a2bac0d16f89a4f6daed
Alexander Wörl <alexander.woerl@contentserv.com>
2025-01-30 22:10:55 +0100

    Updated

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -26,7 +27,7 @@ const GameCard = ({ game }: Props) => {
           textOverflow="ellipsis"
           fontSize="2xl"
         >
-          {game.name}
+          <Link to={"games/" + game.slug}>{game.name}</Link>
           <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,6 +9,9 @@ const apiClient = new APIClient<Game>('/games');
 export interface Game {
   id: number;
   name: string;
+  slug: string; 
+  description: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: {platform: Platform}[];
   metacritic: number;
